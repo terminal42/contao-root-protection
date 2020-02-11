a98c655d3034efc540d9593850ae6ed5afc6e90c
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-02-11 13:11:02 +0100

    Use palette manipulator

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -11,13 +11,14 @@ declare(strict_types=1);
  * @link       http://github.com/terminal42/contao-root-protection
  */
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'rootProtection';
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(
-    ';{publish_legend}',
-    ';{rootProtectionLegend},rootProtection;{publish_legend}',
-    $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
-);
+PaletteManipulator::create()
+    ->addLegend('rootProtection_legend', 'publish_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('rootProtection', 'rootProtection_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('root', 'tl_page');
+
+$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'rootProtection';
 
 $GLOBALS['TL_DCA']['tl_page']['subpalettes']['rootProtection'] = 'rootProtectionUsername,rootProtectionPassword';
 
