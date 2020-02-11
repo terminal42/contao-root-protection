fb4c339329b7f32f8bdbaa9bbf6ddd4f9409be94
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-02-11 11:26:23 +0100

    Fix dca

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -26,7 +26,8 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['rootProtection'] = [
     'exclude'   => true,
     'inputType' => 'checkbox',
     'eval'      => [
-        'tl_class' => 'w50',
+        'tl_class'       => 'w50',
+        'submitOnChange' => true,
     ],
     'sql'       => "char(1) NOT NULL default ''",
 ];
@@ -36,7 +37,8 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['rootProtectionUsername'] = [
     'exclude'   => true,
     'inputType' => 'text',
     'eval'      => [
-        'tl_class' => 'clr w50',
+        'tl_class'  => 'clr w50',
+        'mandatory' => true,
     ],
     'sql'       => "varchar(255) NOT NULL default ''",
 ];
@@ -44,9 +46,10 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['rootProtectionUsername'] = [
 $GLOBALS['TL_DCA']['tl_page']['fields']['rootProtectionPassword'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_page']['rootProtectionPassword'],
     'exclude'   => true,
-    'inputType' => 'checkbox',
+    'inputType' => 'text',
     'eval'      => [
-        'tl_class' => 'w50',
+        'tl_class'  => 'w50',
+        'mandatory' => true,
     ],
     'sql'       => "varchar(255) NOT NULL default ''",
 ];
